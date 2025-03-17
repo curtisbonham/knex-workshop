1444756da3f2ea34b8e6c458b7d3da21caf8a4ac
curtisbonham <curtis.bonham1@gmail.com>
2025-03-17 12:27:26 -0600

    added error catches to gets

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8081;
 const cors = require('cors');
-
 const knex = require('knex')(require('../knexfile')['development']);
-
 app.use(cors());
 app.use(express.json());
 
@@ -18,33 +16,75 @@ app.get('/', (req, res) => {
 
 app.get('/all', (req, res) => {
   knex('employees')
-  .join('contact_info', 'employees.id', '=', 'contact_info.employee_id')
+  .join('contact_info', 'employees.id', '=', 'contact_info.id')
   .select('*')
-  .then(data => res.status(200).json(data));
-});
+  .then(data => res.status(200).json(data))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+  }))
+})
+
 
 app.get('/employees', (req, res) => {
   knex('employees')
   .select('*')
-  .then(data => res.status(200).json(data));
-});
+  .then(data => res.status(200).json(data))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+    }))
+})
+
 
 app.get('/employees/:id', (req, res) => {
   knex('employees')
   .where({ id: req.params.id })
-  .then(data => {
-    res.json(data);
-  });
-});
+  .then(data => res.json(data))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+    }))
+})
 
 app.get('/contact', (req, res) => {
   knex('contact_info')
   .select('*')
-  .then(data => res.json(data));
-});
+  .then(data => res.json(data))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+    }))
+})
+
 
 app.get('/contact/:id', (req, res) => {
   knex('contact_info')
   .where({ employee_id: req.params.id })
-  .then(data => res.json(data));
+  .then(data => res.json(data))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+    }))
+})
+
+app.post('/employees', (req, res) => {
+  const {first_name, last_name} = req.body;
+  knex('employees')
+  .insert(req.body)
+  .then(() => res.status(200).send('Employee added'))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+    }))
+});
+
+app.post('/contact', (req, res) => {
+  knex('contact_info')
+  .insert(req.body)
+  .then(() => res.status(200).send('Contact info added'))
+  .catch(err =>
+    res.status(404).json({
+    message: 'The data you are looking for could not be found. Please try again '
+    }))
 });
